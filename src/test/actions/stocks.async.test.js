4ec3b3f49251dc7c916443212973eb4adc6b885f
Leandro Maríngolo <leandronsp@gmail.com>
2017-10-14 20:32:35 +0100

    refactoring actions stocks test

---
+++ src/test/actions/stocks.async.test.js
@@ -0,0 +1,30 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import nock from 'nock'
+import * as actions from 'actions/stocks.async'
+import types from 'actions/actionTypes'
+import config from 'config'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+it('#fetchStocks', () => {
+  const body = [
+    { symbol: 'PETR4' }
+  ]
+
+  nock(config.API_ENDPOINT)
+    .get('/api/stocks')
+    .reply(200, body)
+
+  const expected = [
+    { type: types.FETCH_STOCKS_REQUEST },
+    { type: types.FETCH_STOCKS_SUCCESS, body: body }
+  ]
+
+  const store = mockStore()
+
+  store.dispatch(actions.fetchStocks()).then(() => {
+    expect(store.getActions()).toEqual(expected)
+  })
+})
