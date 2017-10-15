0e3cfa68478bfcbd9dc2de5e2b30cbe602e2419d
Leandro Maríngolo <leandronsp@gmail.com>
2017-10-15 01:07:32 +0100

    add scenario for stock reducer

--- src/test/reducers/stocks.test.js
+++ src/test/reducers/stocks.test.js
@@ -1,5 +1,15 @@
 import reducer from 'reducers/stocks'
+import types  from 'actions/actionTypes'
 
 it('returns the default state', () => {
-  //expect(reducer(undefined, '')).toEqual({})
+  expect(reducer(undefined, '')).toEqual([])
+})
+
+it('returns the new state for FETCH_STOCKS_SUCCESS', () => {
+  const action = {
+    type: types.FETCH_STOCKS_SUCCESS,
+    body: [{ symbol: 'VALE5' }, { symbol: 'PETR4' }]
+  }
+
+  expect(reducer([{ symbol: 'CIEL3' }], action)).toEqual(action.body)
 })
