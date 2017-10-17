16654d825d9a6b4ac87bf8547e7134e2ee31d168
Leandro Maríngolo <leandronsp@gmail.com>
2017-10-18 00:49:50 +0100

    add snapshot test for component

--- app/components/stock/index.test.js
+++ app/components/stock/index.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
 import Stock from 'components/stock/index'
 
 it('renders stock element', () => {
   const props = { symbol: 'PETR4' }
   const component = shallow(<Stock {...props} />)
   expect(component.find('li').text()).toEqual('PETR4')
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Stock symbol='VALE5' />
+  ).toJSON()
+
+  expect(tree).toMatchSnapshot()
 })
